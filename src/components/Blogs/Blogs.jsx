3839d4b17e6b51b83b5f165de130a96b87b1e6da
Rafsan12 <rafsan.web.2022@gmail.com>
2024-09-15 17:28:21 +0600

    show data in website

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -2,7 +2,18 @@ import { useEffect, useState } from "react";
 export default function Blogs() {
   const [blogs, setBlogs] = useState([]);
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    const blogsData = async () => {
+      try {
+        const res = await fetch("blogs.json");
+        const result = await res.json();
+        console.log(result);
+      } catch (error) {
+        setBlogs(error);
+      }
+    };
+    blogsData();
+  }, []);
 
   return <></>;
 }
