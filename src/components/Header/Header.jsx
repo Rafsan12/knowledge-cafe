9b7e89c4184d240bd930cc9bfd7e1e252b10b857
Rafsan12 <rafsan.web.2022@gmail.com>
2024-09-16 22:08:08 +0600

    title change

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import profile from "../../../images/profile.png";
 export default function Header() {
   return (
     <header className="flex justify-between items-center p-4  border-b-2 max-w-7xl mx-auto ">
-      <h1 className="text-4xl font-bold">knowledge-Cafe</h1>
+      <h1 className="text-4xl font-bold">Blog</h1>
       <img src={profile} alt="" />
     </header>
   );
